b21d1eb72ab966d242f16f7b066586ce6461b693
Yibing Tan <yibtan@thoughtworks.com>
2020-12-10 15:19:37 +0800

    react native web demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ReactNode} from 'react';
+import {View, StyleSheet, Image, Text, Button} from 'react-native';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface LinkProps {
+  style?: any;
+  href: string;
+  children: ReactNode;
 }
 
+const Link = (props: LinkProps) => (
+  <Text
+    {...props}
+    accessibilityRole="link"
+    style={StyleSheet.compose(
+      styles.link,
+      props.style
+    )}
+  />
+);
+
+const App = () => (
+  <View style={styles.app}>
+    <View style={styles.header}>
+      <Image
+        source={require('./logo.svg')}
+        style={styles.logo}
+        resizeMode='contain'
+      />
+      <Text style={styles.title}>React Native for Web</Text>
+    </View>
+    <Text style={styles.text}>
+      This is an example of an app built with{" "}
+      <Link href="https://github.com/facebook/create-react-app">
+        Create React App
+      </Link>{" "}
+      and{" "}
+      <Link href="https://github.com/necolas/react-native-web">
+        React Native for Web
+      </Link>
+    </Text>
+    <Text style={styles.text}>
+      To get started, edit{" "}
+      <Link href="https://codesandbox.io/s/q4qymyp2l6/" style={styles.code}>
+        src/App.js
+      </Link>
+      .
+    </Text>
+    <Button onPress={() => console.log('clicked')} title='React Native Web Button' />
+  </View>
+);
+
 export default App;
+
+const styles = StyleSheet.create({
+  app: {
+    marginHorizontal: "auto",
+    maxWidth: 500
+  },
+  logo: {
+    height: 80
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginVertical: 16,
+    textAlign: "center"
+  },
+  text: {
+    lineHeight: 24,
+    fontSize: 18,
+    marginVertical: 16,
+    textAlign: "center"
+  },
+  link: {
+    color: "#1B95E0"
+  },
+  code: {
+    fontFamily: "monospace, monospace"
+  }
+});
